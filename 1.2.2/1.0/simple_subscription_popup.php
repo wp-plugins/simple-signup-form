985f9a2d376eb736986a254338228f7542cbb292
pantherius <pantherius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-22 09:09:30 +0000

    fixed minor bug with triggering the popup via link
    git-svn-id: https://plugins.svn.wordpress.org/simple-signup-form/tags@1073261 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.2.2/1.0/simple_subscription_popup.php
@@ -0,0 +1,159 @@
+<?php
+defined( 'ABSPATH' ) OR exit;
+/**
+ * Plugin Name: Simple Signup Form Free
+ * Plugin URI: http://sympies.com/simple_popup_signup_form/
+ * Description: Easily add and manage signup forms
+ * Author: Pantherius
+ * Version: 1.0
+ * Author URI: http://sympies.com
+ */
+ 
+if(!class_exists('simple_signup_form'))
+{
+	class simple_signup_form
+	{
+		protected static $instance = null;
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct()
+		{
+			global $wpdb;
+			// installation and uninstallation hooks
+			register_activation_hook(__FILE__, array('simple_signup_form', 'activate'));
+			register_deactivation_hook(__FILE__, array('simple_signup_form', 'deactivate'));
+			register_uninstall_hook(__FILE__, array('simple_signup_form', 'uninstall'));
+			if (is_admin())
+			{
+				require_once(sprintf("%s/settings.php", dirname(__FILE__)));
+				$simple_signup_form_settings = new simple_signup_form_settings();
+				$plugin = plugin_basename(__FILE__);
+				add_filter("plugin_action_links_$plugin", array(&$this, 'plugin_settings_link'));
+			}
+			else
+			{
+				$simple_signup_form_url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+				$simple_signup_form_load = true;
+				if ((strpos($simple_signup_form_url,'wp-login'))!==false) {$simple_signup_form_load = false;}
+				if ((strpos($simple_signup_form_url,'wp-admin'))!==false) {$simple_signup_form_load = false;}
+				if ($simple_signup_form_load||isset($_REQUEST['sspcmd'])) 
+				{
+					//integrate the public functions
+					add_action('init', array(&$this, 'enqueue_custom_scripts_and_styles'));
+					add_shortcode( 'ssf', array( &$this, 'ssf_shortcodes' ) );
+				}
+			}
+		}
+		public static function getInstance()
+		{
+			if (!isset($instance)) 
+			{
+				$instance = new simple_signup_form;
+			}
+		return $instance;
+		}
+		/**
+		* Activate the plugin
+		**/
+		public static function activate()
+		{
+			global $wpdb;
+			$db_info = array();
+			//define custom data tables
+			$charset_collate = '';
+			if ( ! empty( $wpdb->charset ) ) {
+			  $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
+			}
+
+			if ( ! empty( $wpdb->collate ) ) {
+			  $charset_collate .= " COLLATE {$wpdb->collate}";
+			}
+			//creating custom tables
+			$sql = "CREATE TABLE IF NOT EXISTS ".$wpdb->prefix.'simple_subscription_popup'." (
+			  id varchar(255) NOT NULL,
+			  name varchar(255) NOT NULL,
+			  options text NOT NULL,
+			  global tinyint(1) NOT NULL,
+			  autoid mediumint(9) NOT NULL AUTO_INCREMENT,
+			  UNIQUE KEY autoid (autoid)
+			) $charset_collate";
+			$wpdb->query($sql);
+		}
+		/**
+		* Deactivate the plugin
+		**/
+		public static function deactivate()
+		{
+			wp_unregister_sidebar_widget('simple_signup_form');
+		}
+		
+		/**
+		* Uninstall the plugin
+		**/
+		public static function uninstall()
+		{
+			global $wpdb;
+			$db_info = array();
+			//define custom data tables
+			$wpdb->query("DROP TABLE IF EXISTS ".$wpdb->prefix.'simple_subscription_popup');
+		}
+			
+		public function ssf_shortcodes( $atts ) {
+			global $wpdb;
+			extract( shortcode_atts( array(
+					'id' => '-1'
+				), $atts, 'ssf' ) );
+				$args = array('id' => $atts['id']);
+				if (!is_single()&&!is_page()) return('');
+			wp_deregister_script( 'simple_signup_form_script' );
+			wp_dequeue_style( 'simple_signup_form_script' );
+			wp_register_script('simple_signup_form_script', plugins_url('/templates/assets/js/ssf.min.js' , __FILE__ ), array('jquery'),'1.0.0.2',true );
+			$sql = "SELECT * FROM ".$wpdb->prefix."simple_subscription_popup ssp WHERE ssp.id='".$args['id']."' ORDER BY ssp.id ASC";
+			$ssp_sql = $wpdb->get_results($sql);
+			if (!empty($ssp_sql))
+			{
+			$ssp_options = json_decode(stripslashes($ssp_sql[0]->options));
+			if ($ssp_options[10]) $ssp_options[18] = $ssp_options[18]*1000;
+			$ssp_options[100] = admin_url( 'admin-ajax.php');
+			$ssp_options[101] = $ssp_sql[0]->id;
+			wp_localize_script('simple_signup_form_script', 'passed', $ssp_options);
+			wp_enqueue_script('simple_signup_form_script');
+			}
+		}
+			
+		function enqueue_custom_scripts_and_styles() 
+		{
+			global $wpdb;
+			wp_enqueue_style('simple_signup_form_style', plugins_url( '/templates/assets/css/ssf.css' , __FILE__ ));
+			wp_enqueue_script('jquery');
+			wp_register_script('simple_signup_form_script', plugins_url('/templates/assets/js/ssf.min.js' , __FILE__ ), array('jquery'),'1.0.0.1',true );
+			$sql = "SELECT * FROM ".$wpdb->prefix."simple_subscription_popup ssp WHERE global = 1 ORDER BY ssp.id ASC";
+			$ssp_sql = $wpdb->get_results($sql);
+			if (!empty($ssp_sql))
+			{
+			$ssp_options = json_decode(stripslashes($ssp_sql[0]->options));
+			if ($ssp_options[10]) $ssp_options[18] = $ssp_options[18]*1000;
+			$ssp_options[100] = admin_url( 'admin-ajax.php');
+			$ssp_options[101] = $ssp_sql[0]->id;
+			wp_localize_script('simple_signup_form_script', 'passed', $ssp_options);
+			wp_enqueue_script('simple_signup_form_script');
+			}
+		}
+		/**
+		* Add the settings link to the plugins page
+		**/
+		function plugin_settings_link($links)
+		{ 
+			$settings_link = '<a href="options-general.php?page=simple_signup_form">Settings</a>';
+			array_unshift($links, $settings_link); 
+			return $links; 
+		}
+	}
+}
+if(class_exists('simple_signup_form'))
+{
+	// call the main class
+	$simple_signup_form = simple_signup_form::getInstance();
+}
+?>
---
+++ 1.2.2/1.1/settings.php
@@ -0,0 +1,185 @@
+<?php
+if(!class_exists('simple_signup_form_settings'))
+{
+	class simple_signup_form_settings extends simple_signup_form
+	{
+	/**
+	* Construct the plugin object
+	**/
+		public function __construct()
+		{
+		global $wpdb;
+		$this->wpdb =& $wpdb;
+		/**
+		* register actions, hook into WP's admin_init action hook
+		**/
+		add_action('admin_init', array(&$this, 'admin_init'));
+		add_action('admin_menu', array(&$this, 'add_menu'));
+		add_action('wp_ajax_ajax_ssp', array(&$this, 'ajax_ssp'));
+		add_action('wp_ajax_nopriv_ajax_ssp', array(&$this, 'ajax_ssp'));
+		}
+		/**
+		* include custom scripts and style to the admin page
+		**/
+		function enqueue_admin_custom_scripts_and_styles() {
+			wp_enqueue_style('simple_signup_form_admin_style', plugins_url( '/templates/assets/css/ssf_settings.css' , __FILE__ ));
+			wp_enqueue_style('simple_signup_form_style', plugins_url( '/templates/assets/css/ssf.css' , __FILE__ ));
+			wp_enqueue_style('simple_signup_form_ui_style', plugins_url( '/templates/assets/css/jquery-ui.css' , __FILE__ ));
+			wp_enqueue_script('jquery');
+			wp_enqueue_script('jquery-ui-core',array('jquery'));
+			wp_enqueue_script('jquery-ui-tabs',array('jquery-ui-core'));
+			wp_enqueue_script('jquery-ui-slider',array('jquery-ui-core'));
+			wp_enqueue_script('jquery-ui-tooltip',array('jquery-ui-core'));
+			wp_enqueue_script('jquery-ui-accordion',array('jquery-ui-core'));
+			wp_enqueue_script('jquery-ui-dialog',array('jquery-ui-core'));
+			wp_enqueue_script('jquery-effects-core',array('jquery'));
+			wp_enqueue_script('jquery-effects-fade',array('jquery-effects-core'));
+			wp_register_script('simple_signup_form_admin', plugins_url( '/templates/assets/js/ssf_admin.js' , __FILE__ ) , array('jquery','jquery-ui-core','jquery-effects-core'),'100018', true);
+			wp_enqueue_script('simple_signup_form_script', plugins_url( '/templates/assets/js/ssf.min.js' , __FILE__ ) , array('jquery','jquery-ui-core','jquery-effects-core'),'100018');
+			wp_localize_script( 'simple_signup_form_admin', 'sspa_params', array( 'plugin_url'=>plugins_url( '' , __FILE__ ), 'admin_url'=>admin_url( 'admin-ajax.php')));
+			wp_enqueue_script( 'simple_signup_form_admin' );
+			wp_enqueue_script( "simple_signup_form_colorpicker_script", plugins_url('/templates/assets/js/colorpicker.js' , __FILE__ ));
+			wp_enqueue_style('simple_signup_form_colorpicker_style', plugins_url( '/templates/assets/css/colorpicker.css' , __FILE__ ));
+		}
+		/**
+		* initialize datas on wp admin
+		**/
+		public function admin_init()
+		{
+		$settings_page = '';
+			if (isset($_REQUEST['page'])) $settings_page = sanitize_title($_REQUEST['page']);
+			if ($settings_page=='simple_signup_form') add_action('admin_head', array(&$this, 'enqueue_admin_custom_scripts_and_styles'));
+			// Possibly do additional admin_init tasks
+		}
+		/**
+		* add a menu
+		**/		
+		public function add_menu()
+		{
+			// Add a page to manage this plugin's settings
+			add_options_page('Simple Signup Form FREE', 'Simple Signup Form FREE', 'manage_options', 'simple_signup_form', array(&$this, 'plugin_settings_page'));
+		}
+		/**
+		* Menu Callback
+		**/		
+		public function plugin_settings_page()
+		{
+			if(!current_user_can('manage_options'))
+			{
+				wp_die(__('You do not have sufficient permissions to access this page.'));
+			}
+			// Render the settings template
+			include(sprintf("%s/templates/settings.php", dirname(__FILE__)));
+		}
+		public function settings_section_simple_signup_form()
+		{
+		
+		}
+
+		public function ajax_ssp()
+		{
+		global $wpdb;
+		$signup_form_id = "";
+		$form_name = "";
+		$form_options = "";
+		$form_global = "";
+		if (isset($_REQUEST['signup_form_id'])) $signup_form_id = sanitize_text_field($_REQUEST['signup_form_id']);
+		else $signup_form_id = "";
+		if (isset($_REQUEST['form_name'])) $form_name = sanitize_text_field($_REQUEST['form_name']);
+		else $form_name = "";
+		if (isset($_REQUEST['global_use'])) $form_global = sanitize_text_field($_REQUEST['global_use']);
+		else $form_global = "";
+		if (isset($_REQUEST['options'])) $form_options = sanitize_text_field($_REQUEST['options']);
+		else $form_options = "";
+		if (!empty($signup_form_id)&&($signup_form_id>0)) $form_check = $wpdb->get_var("SELECT COUNT(*) FROM ".$wpdb->prefix."simple_subscription_popup WHERE `id` = ".$signup_form_id);
+		else $form_check = 0;
+		if ($_REQUEST['sspcmd']=="save")
+		{
+		if ($form_check>0) {
+		//update signup form
+			$wpdb->update( $wpdb->prefix."simple_subscription_popup", array( "options" => $form_options, 'global' => $form_global),array('id' => $signup_form_id));
+			die("updated");
+		}
+		else {
+		//insert signup form
+			$wpdb->insert( $wpdb->prefix."simple_subscription_popup", array( 
+				'id' => $signup_form_id, 
+				'name' => $form_name, 
+				'options' => $form_options, 
+				'global'=> $form_global
+				) );
+			die('success');
+		}
+		}
+		elseif($_REQUEST['sspcmd']=="delete")
+		{
+		if ($form_check>0) {
+			$wpdb->query($wpdb->prepare("DELETE FROM ".$wpdb->prefix."simple_subscription_popup WHERE `id` = %d",$signup_form_id));
+			die("deleted");
+		}
+		}
+		elseif ($_REQUEST['sspcmd']=="subscription_signup")
+		{
+			$form_options = "";
+			if (isset($_REQUEST['email'])) $email = sanitize_email($_REQUEST['email']);
+			else $email = "";
+			if (isset($_REQUEST['mode'])) $mode = sanitize_text_field($_REQUEST['mode']);
+			else $mode = "";
+			if (isset($_REQUEST['double_optin'])) $double_optin = sanitize_text_field($_REQUEST['double_optin']);
+			else $double_optin = "";
+			if (isset($_REQUEST['update_existing'])) $update_existing = sanitize_text_field($_REQUEST['update_existing']);
+			else $update_existing = "";
+			if (isset($_REQUEST['replace_interests'])) $replace_interests = sanitize_text_field($_REQUEST['replace_interests']);
+			else $replace_interests = "";
+			if (isset($_REQUEST['mailchimp_listid'])) $mailchimp_listid = sanitize_text_field($_REQUEST['mailchimp_listid']);
+			else $mailchimp_listid = "";
+			if (isset($_REQUEST['send_welcome'])) $send_welcome = sanitize_text_field($_REQUEST['send_welcome']);
+			else $send_welcome = "";
+			$customfields = '';
+			if (isset($_REQUEST['customfieldsarray']))
+			{
+				if (!empty($_REQUEST['customfieldsarray']))
+				{
+					foreach($_REQUEST['customfieldsarray'] as $cfa)
+					{
+						$mv[$cfa] = sanitize_text_field($_REQUEST[$cfa]);
+						$customfields .='
+'.sanitize_text_field($cfa).': '.sanitize_text_field($_REQUEST[$cfa]);
+					}
+				}
+			}
+			else $mv = '';
+			if (!empty($signup_form_id)&&($signup_form_id>0))
+			{
+			$form_check = $wpdb->get_var("SELECT options FROM ".$wpdb->prefix."simple_subscription_popup WHERE `id` = ".$signup_form_id);
+			if (!empty($form_check)) $form_options = json_decode(stripslashes($form_check));
+			}
+			if (empty($form_check)&&!empty($signup_form_id)) $form_options[35] = sanitize_email($signup_form_id);
+			if (!isset($form_options[35])) die('error');
+			else
+			{
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) die('error');
+				else
+				{
+				if ($mode=='mail')
+				{
+					$body = "You've got a new signup on the http://".$_SERVER['HTTP_HOST'].str_replace('/wp-admin/admin-ajax.php','',$_SERVER['REQUEST_URI'])." website with the following mail address: ".$email.$customfields."
+					
+					";
+					$from_a = 'noreply@'.$_SERVER["HTTP_HOST"];
+					$from_name = 'Simple Signup Form';
+					$header = 'MIME-Version: 1.0' . "\r\n";
+					$header .= "From: =?utf-8?b?".base64_encode($from_name)."?= <".$from_a.">\r\n";
+					$header .= 'Content-type: text/plain; charset=UTF-8' . "\r\n";
+					if (mail($form_options[35], 'SUBSCRIPTION SIGNUP', $body, $header)) $result = true;
+					else $result = false;
+				}
+				if ($result==true) die("success");
+				else die("error");
+				}			
+			}
+			}
+			}
+	}
+}
+?>
