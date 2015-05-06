747c92cb2a4e7ad19419946143e4401d1307b4a8
pantherius <pantherius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 08:16:55 +0000

    fixed animation bug in Explorer
    git-svn-id: https://plugins.svn.wordpress.org/simple-signup-form/trunk@1154370 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/acustom.php
+++ templates/acustom.php
@@ -3,7 +3,7 @@ print('
 <div class="pop-up-screen">
 	 <div class="pop-up-content">
 		<h3>Thank you...</h3>
-		<p>for downloading this plugin. Simple Signup Form Free is a limited version, even you can reach many of options. If you really would like to get much more customization possibilities, purchase the <a target="_blank" href="http://pantherius.com/simple-signup-form/">PRO version</a> for only a one-time fee: $18</p>
+		<p>for downloading this plugin. Simple Signup Form Free is a limited version, even you can reach many of options. If you really would like to get much more customization possibilities, purchase the <a target="_blank" href="http://simplesignupform.pantherius.com/">PRO version</a> for only a one-time fee: $18</p>
 		<blockquote>This is a solid solution! It is very effective too! I was getting 1 newsletter sign up every 1.9 days. I am now consistently getting 10 email sign ups every day. This is with about 250 visits per day. Full disclosure my solution before required customers to register now they can signup without having to!<cite>jeremy11allen 
 <img src="'.plugins_url( '/assets/img/fivestar.jpg' , __FILE__ ).'"/>
 
@@ -12,12 +12,12 @@ print('
 		<div class="half">
 		<ul>
 			<li>- Integration with MailChimp, Active Campaign, Benchmark, Campaign Monitor, Campayn, Constant Contact, Freshmail, GetResponse, Mad Mimi, MyMail, SimplyCast, YMLP</li>
-			<li>- Subscription with Google Plus & Facebook</li>
-			<li>- HTML content in the form, YouTube Integration</li>
-			<li>- Bottom line text and removed copyright notice</li>
-			<li>- More animation effects and flexible positions</li>
-			<li>- Unlimited custom text fields, Tracking Forms with Google Analytics</li>
-			<li>- Free Support & Updates</li>
+			<li>- Subscription with Google Plus & Facebook, <strong>Embed Mode</strong></li>
+			<li>- HTML content in the form, YouTube Integration, Multiple Instances</li>
+			<li>- Bottom line text and removed copyright notice, <strong>200+ Animation Variations</strong></li>
+			<li>- Custom Radio Buttons, Select Field, Textarea, Video Background</li>
+			<li>- Unlimited custom text fields, Tracking Forms with <strong>Google Analytics</strong></li>
+			<li>- Premium Support & Updates</li>
 		</ul>
 		</div>
 		<div class="half">
